a72ab331dcec9a54b3a4f027413dd1ac3979035c
Zoe Roux <zoe.roux@zoriya.dev>
2024-06-30 09:38:10 +0000

    Move video ref type to its own file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import Video from './Video';
 export {VideoDecoderProperties} from './VideoDecoderProperties';
 export type * from './types';
-export type {VideoRef} from './Video';
 export default Video;
